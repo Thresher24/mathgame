945e3365d970d306b50a00b578a50b796dc58d69
Thresher24 <darkthresher24@gmail.com>
2016-11-29 17:58:26 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,23 +1,181 @@
-<html>
-<head>
-    <title>Math Game</title>
-</head>
-<body>
-    <div class = "container">
-        <form action = "index.php" method = "post" role = "form" class = "form-horizontal">
-            <div class = "row">
-                <div class = "col-sm-4 col-sm-offset-4">
-                    <h1>Math Game</h1>
-                </div>
-                <div class = "col-sm-4">
-                    <a href = "logout.php" class = "btn btn-default btn-sm">Logout</a>
-                </div> 
+<?php session start();
+extract($_POST);
+$error = "";
+
+// login - wrong
+if($_SESSION["login"] == false)
+    if(!isset($email)&& !isset($password)) {
+        header(Location: login.php)
+        die();
+    } else if (strcmp($email, "i@i.i") != 0 || strcmp($password, "iii") != 0) {
+        $error = "Incorrect login credentials.";
+    }
+    if (!empty($error)) {
+        unset($_SESSION["email"]);
+        unset($_SESSION["password"]);
+        header("Location: login.php?msg=$error");
+        die();
+    }
+}
+
+// login - right
+$_SESSION["login"] = true;
+$_SESSION["email"] = $email;
+$_SESSION["password"] = $password;
+
+include("include/header.php");
+
+// Mathgame Code
+$_SESSION["score"];
+if ($_SESSION["score"] == null) {
+    $_SESSION["score"] = 0;
+    $_SESSION["Scorecount"] = 0;
+}
+
+// Fractions
+list($up, $down) = explode('/', $answer);
+if ($down == 0) {
+    $fraction = 0;
+} else {
+    $fraction = $up / $down;
+}
+
+// Mathgame Operators (+,-,*,/)
+if ($_SESSION["answer"] == $answer || $fraction == $_SESSION["num1"] / $_SESSION["num2"]) {
+    $_SESSION["score"]++;
+    $_SESSION["count"]++;
+    $mathout = "Correct";
+    $correct = true;
+    
+    //right
+} else if ((!is_numeric($answer) || empty($answer)) && $answer != 0) {
+    $mathout = "It is required that you input your answer";
+    $correct = false;
+    
+    //wrong
+} else {
+    $correct = false;
+    $_SESSION["count"]++;
+    $num1 = $_SESSION["num1"];
+    $num2 = $_SESSION["num2"];
+    $mathout = "INCORRECT, " . $_SESSION["num1"] . " ";
+    
+    // add
+    if ($_SESSION["operator"] == 0) { 
+        $answer = $num1 + $num2;
+        $sign = "+";
+    // minus
+        
+    } else if ($_SESSION["operator"] == 1) { 
+        $answer = $num1 - $num2;
+        $sign = "-";
+     // multiply
+        
+    } else if ($_SESSION["operator"] == 2) {
+        $answer = $num1 * $num2;
+        $sign = "x";
+    // divide
+        
+    } else if ($_SESSION["operator"] == 3) { 
+        $answer = $num1 / $num2;
+        $sign = "&#247;";
+    }
+    $mathout .= $sign . " " . $_SESSION["num2"] . " is " . $answer . ".";
+}
+
+// Random Question
+$num1 = rand(0, 20);
+$num2 = rand(0, 20);
+$operator = rand(0, 3);
+
+// add
+if ($operator == 0) { 
+    $answer = $num1 + $num2;
+    $sign = "+";
+// minus
+    
+} else if ($operator == 1) { 
+    $answer = $num1 - $num2;
+    $sign = "-";
+ // multiply
+    
+} else if ($operator == 2) {
+    $answer = $num1 * $num2;
+    $sign = "x";
+// divide
+    
+} else if ($operator == 3) { 
+    $answer = $num1 / $num2;
+    $sign = "&#247;";
+}
+
+$_SESSION["answer"] = $answer;
+$_SESSION["num1"] = $num1;
+$_SESSION["num2"] = $num2;
+$_SESSION["operator"] = $operator;
+
+?>
+
+<form class = "form-horizontal" action = "login.php" method = "post">
+    <div class = "row">
+        <div class = "Col-sm-4 col-sm-offset-4">
+            <h1>Math Game</h1>
+        </div>
+        <div class = "col-sm-4">
+            <div class = "form-group">
+                <button type = "logout" href = "login.php?msg=&quot;logout&quot;" class = "btn btn-default pull-right" name = "logoutbutton">Logout</button>
             </div>
-            <div class = "row">
-                <label class = "col-sm-2 col-sm-offset-3"></label>
-            </div>
-            <label type = "hidden" name = "1" value = "2"></label>
-        </form>
+        </div>
     </div>
-</body>
-</html>
+</form>
+<form class="form-horizontal" action="index.php" method="post">
+    <div class="row">
+        <label class="col-sm-1 text-right col-sm-offset-6">
+            <?php echo $num1 ?>
+        </label>
+    </div>
+    <div class="row">
+        <label class="col-sm-1 col-sm-offset-5">
+            <?php echo $sign ?>
+        </label>
+        <label class="col-sm-1 text-right">
+            <?php echo $num2 ?>
+        </label>
+    </div>
+    <div class="form-group">
+        <div class="row">
+            <div class="col-sm-2 col-sm-offset-5">
+                <hr>
+                <input type="text" value="" class="form-control" id="answer" name="answer" placeholder="answer" align="right" autofocus></input>
+            </div>
+        </div>
+    </div>
+<div class="form-group">
+    <div class="col-sm-offset-5 text-center col-sm-2">
+        <button type="submit" class="btn btn-success" name="submit">Submit</button>
+    </div>
+</div>
+</form>
+<div class="row">
+    <div class="col-sm-offset-3 text-center col-sm-6">
+        <label>
+            <?php
+            if ($correct) {
+                echo "<span id=\"correct\" >";
+            } else {
+                echo "<span id=\"incorrect\" >";
+            }
+                    echo $mathout . "</span>";
+            ?> 
+        </label>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-offset-5 text-center col-sm-2">
+        <label>
+            <?php
+            echo "Score: " . $_SESSION["score"] . " / " . $_SESSION["count"];
+            ?>
+        </label>
+    </div>
+</div>
--- login.php
+++ login.php
@@ -1,36 +1,58 @@
-<html>
-<head>
-    <title>Math Game</title>
-</head>
-<body>
-    <div class = "container">
-        <div class = "row">
-            <div class = "col-sm-10 col-sm-offset-1">
-                <h1>Log in to play the Math Game</h1>
-            </div>
-            <div class = "col-sm-1"></div>
+<?php
+session_start();
+$msg = $_GET["msg"];
+?>
+
+<h2>Please login to enjoy our Math Game.</h2>
+
+<?php 
+
+    $_SESSION["login"] = false;
+    $_SESSION["email"] = "";
+    $_SESSION["password"] = "";
+    $_SESSION["count"] = 0;
+    $_SESSION["score"] = 0;
+
+if(!empty($msg)) {
+    
+    echo "<div class=\"alert alert-dismissible alert-danger\">
+  <button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>
+  <strong>Whoops!</strong> " . $msg . "</div>"
+}
+
+?>
+
+<form class = "form-horizontal" action = "index.php" method = "post">
+    <div class = "form-group">
+        <div class = "col-sm-3">
+            <label class = "control" for = "email">Email:</label>
         </div>
-        <form action = "check.php" method = "post" role = "form" class = "form-horizontal">
-            <div class = "form-group">
-                <div class = "col-sm-4 text-right">Email:</div>
-                <div class = "col-sm-3">
-                    <input type = "text" class = "form-control" id = "email" name = "email" placeholder = "Email" size = "6">
-                </div>
-                <div class = "col-sm-5"></div>
+        <div class = "col-sm-9 form-group-req">
+            <div>
+                <input type = "email" class = "form-control" id = "email" name = "email" placeholder = "i@i.i">
+            </div> 
+        </div>
+        <div class = "form-group">
+            <div class = "col-sm-3">
+                <label class = "control" for = "password">Password</label>
             </div>
-            <div class = "form-group">
-                <div class = "col-sm-4 text-right">Password</div>
-                <div class = "col-sm-3">
-                    <input type = "text" class = "form-control" id = "password" name = "password" placeholder = "Password" size = "6">
-                </div>
-                <div class = "col-sm-5"></div>
-            </div>
-            <div class = "row">
-                <div class = "col-sm-3 col-sm-offset-4">
-                    <button type = "submit" class = "btn btn-primary">Submit</button>
+            <div class = "col-sm-9 form-group-req">
+                <div>
+                    <input type = "password" class = "control" id = "password" name = "password" placeholder = "iii">
                 </div>
             </div>
-        </form>
+        </div>
+        <div class = "form-group">
+            <div class = "col-sm-3 col-sm-offset-9">
+                <div>
+                    <button type = "submit" class = "btn btn-default" name = "submit">
+                        Login
+                    </button>
+                    <button type = "password" class = "btn btn-default" name  = "reset">
+                        Clear Page
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
-</body>
-</html>
+</form>
