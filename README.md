ab10aadbf7ab3d22584bf9ae8f64916c9fb16de5
Thresher24 <darkthresher24@gmail.com>
2016-11-29 18:40:46 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# mathgame" 
+Austin Imbeau , A00984264 , Set 1E 
+Website Link : 
---
+++ include/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Math Game</title>
+        <link href = "stylesheet.css" rel = "stylesheet" media = "screen">
+        <link rel = "stylesheet" href = "style.css" type = "text/css">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+        <meta charset = "utf-8"/>
+
+        <script src = "js/jquery.min.js"></script>
+        <script src = "js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class = "container">
